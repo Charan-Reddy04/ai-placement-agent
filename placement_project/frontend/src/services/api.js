b0c754ead6d1d91728adb516f307e18357225626
Charan-Reddy04 <singamcherry571@gmail.com>
2026-08-29 10:32:44 +0530

    Update api.js

--- placement_project/frontend/src/services/api.js
+++ placement_project/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://ai-placement-agent-6gtt.onrender.com/api"
+  baseURL:"https://ai-placement-agent-6gtt.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
